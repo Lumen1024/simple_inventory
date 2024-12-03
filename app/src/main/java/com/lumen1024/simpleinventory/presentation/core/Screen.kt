bf202ac51f6ca302911541497560422810d9ca8e
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-12-03 17:37:53 +0700

    ref: serializable Screen.kt

--- app/src/main/java/com/lumen1024/simpleinventory/presentation/core/Screen.kt
+++ app/src/main/java/com/lumen1024/simpleinventory/presentation/core/Screen.kt
@@ -1,8 +1,13 @@
 package com.lumen1024.simpleinventory.presentation.core
 
+import kotlinx.serialization.Serializable
+
 sealed interface Screen {
+
     @Serializable
     object ItemsList : Screen
+    @Serializable
     object ModifyItem : Screen
+    @Serializable
     object Settings : Screen
 }
