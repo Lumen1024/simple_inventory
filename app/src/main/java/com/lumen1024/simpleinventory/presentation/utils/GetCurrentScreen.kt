9de8452819eea4a1b5d439426da0e01896dc7e1e
Lumen <123202753+Lumen1024@users.noreply.github.com>
2024-12-04 15:50:43 +0700

    feat: GetCurrentScreen.kt

---
+++ app/src/main/java/com/lumen1024/simpleinventory/presentation/utils/GetCurrentScreen.kt
@@ -0,0 +1,32 @@
+package com.lumen1024.simpleinventory.presentation.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import com.lumen1024.simpleinventory.presentation.core.Screen
+
+@Composable
+fun NavHostController.getCurrentScreenAsState(): State<Screen?> {
+
+    val state = this.currentBackStackEntryFlow
+        .collectAsState(null)
+    return remember {
+        derivedStateOf {
+            state.value?.destination?.getScreen()
+        }
+    }
+}
+
+fun NavDestination.getScreen(): Screen? {
+    val screens = mapOf(
+        Screen.ItemsList.javaClass.canonicalName to Screen.ItemsList,
+        Screen.ModifyItem.javaClass.canonicalName to Screen.ModifyItem,
+        Screen.Settings.javaClass.canonicalName to Screen.Settings,
+    )
+    val route = this.route ?: return null
+    return screens[route]
+}
